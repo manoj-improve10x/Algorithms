46f335b5d508449abbdd529605c36de454745bfc
manoj <manoj.mopoori@improve10x.com>
2023-02-22 15:49:45 +0530

    implemented IntegerInRangeActivity functionality

--- app/src/main/java/com/example/algorithms/IntegerInRangeActivity.java
+++ app/src/main/java/com/example/algorithms/IntegerInRangeActivity.java
@@ -3,12 +3,42 @@ package com.example.algorithms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.algorithms.databinding.ActivityIntegerInRangeBinding;
 
 public class IntegerInRangeActivity extends AppCompatActivity {
+
+    private ActivityIntegerInRangeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_integer_in_range);
+        binding = ActivityIntegerInRangeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().setTitle("FindIntegerRange");
+        findIntegerInRange();
+    }
+
+    private void findIntegerInRange() {
+        binding.submitButton.setOnClickListener(v -> {
+            String inputNumberTxt = binding.inputNumber.getText().toString();
+            int inputNumber = Integer.parseInt(inputNumberTxt);
+            String lowerBoundTxt = binding.lowerBound.getText().toString();
+            int lowerBound = Integer.parseInt(lowerBoundTxt);
+            String upperBoundTxt = binding.upperBound.getText().toString();
+            int upperBound = Integer.parseInt(upperBoundTxt);
+            boolean result = findIntegerRange(inputNumber, lowerBound, upperBound);
+            Toast.makeText(this, "result is: " + result, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private static boolean findIntegerRange(int n, int lowerBound, int upperBound) {
+        boolean result = false;
+        if (n >= lowerBound && n < upperBound) {
+            result = true;
+        }
+        System.out.println(result);
+        return result;
     }
 }
