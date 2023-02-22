39a242981fab4630d19899d2faebc0e61f3455e6
manoj <manoj.mopoori@improve10x.com>
2023-02-22 12:57:07 +0530

    implemented findDiscountActivity design

---
+++ app/src/main/java/com/example/algorithms/FindTheDiscountActivity.java
@@ -0,0 +1,14 @@
+package com.example.algorithms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindTheDiscountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_the_discount);
+    }
+}
