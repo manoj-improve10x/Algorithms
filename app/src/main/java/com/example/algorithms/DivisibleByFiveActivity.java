f743c812a80fcbe91d429f0700ce9fcd85cb712b
manoj <manoj.mopoori@improve10x.com>
2023-02-22 11:13:13 +0530

    implemented Divisible By Five ScreenDesign

---
+++ app/src/main/java/com/example/algorithms/DivisibleByFiveActivity.java
@@ -0,0 +1,14 @@
+package com.example.algorithms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DivisibleByFiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_divisible_by_five);
+    }
+}
