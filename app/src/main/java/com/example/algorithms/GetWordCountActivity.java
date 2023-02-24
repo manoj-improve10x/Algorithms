efaf0890ea01efac53bb3bdb3efb1df04f5f00fd
manoj <manoj.mopoori@improve10x.com>
2023-02-24 14:17:41 +0530

    implemented The GetWordCountActivity Functionality

--- app/src/main/java/com/example/algorithms/GetWordCountActivity.java
+++ app/src/main/java/com/example/algorithms/GetWordCountActivity.java
@@ -3,13 +3,34 @@ package com.example.algorithms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.algorithms.databinding.ActivityGetWordCountBinding;
 
 public class GetWordCountActivity extends AppCompatActivity {
+
+    private ActivityGetWordCountBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_get_word_count);
+        binding = ActivityGetWordCountBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         getSupportActionBar().setTitle("Get Word Count");
+        findWordCount();
     }
+
+    private void findWordCount() {
+        binding.submitBtn.setOnClickListener(v -> {
+            String inputTxt = binding.inputTxt.getText().toString();
+            int wordCount = getWordCount(inputTxt);
+            Toast.makeText(this, String.valueOf(wordCount), Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private static int getWordCount(String names) {
+        String[] wordCount = names.split(" ");
+        return wordCount.length;
+    }
+
 }
