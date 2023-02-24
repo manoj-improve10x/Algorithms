afb0517a429b20984aeb2a4143cc327fc56e323b
manoj <manoj.mopoori@improve10x.com>
2023-02-24 14:36:15 +0530

    implemented The ReverseOrderActivity functionality

--- app/src/main/java/com/example/algorithms/ReverseOrderActivity.java
+++ app/src/main/java/com/example/algorithms/ReverseOrderActivity.java
@@ -3,12 +3,36 @@ package com.example.algorithms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.algorithms.databinding.ActivityReverseOrderBinding;
 
 public class ReverseOrderActivity extends AppCompatActivity {
+
+    private ActivityReverseOrderBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reverse_order);
+        binding = ActivityReverseOrderBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().setTitle("ReverseOrderOfString");
+        getReverseOrderString();
+    }
+
+    private void getReverseOrderString() {
+        binding.submitBtn.setOnClickListener(v -> {
+            String inputText = binding.inputTxt.getText().toString();
+            String output = getReverseOrderOfString(inputText);
+            binding.outputTxt.setText(output);
+        });
+    }
+
+    private static String getReverseOrderOfString(String words) {
+        String stsRv = "";
+        for (int i = words.length() - 1; i >= 0; i--)
+            stsRv += words.charAt(i);
+        System.out.println(stsRv);
+        return stsRv;
     }
 }
